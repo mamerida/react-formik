fa10f6b27669a9bb67faa2b4af5f070f059a2a33
Mario <mamerida2013@gmail.com>
2021-12-27 16:19:45 -0300

    creacion de componentes formik personalizados

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 //vamos a utilizar todas las dependencias que nos entrega formik para realizar nuestro formulario
 import {Formik ,Form,Field,ErrorMessage} from 'formik'
+import TextInput from './components/TextInput'
 
 
 
@@ -31,27 +32,29 @@ function App() {
         validate={validate}
         onSubmit={values => console.log(values)}
     >
-        {formik =>
+        {formik => 
                     // reemplazamos lo generado por etiquetas propias de formik, como son Form Fiel y ErrorMessage
                     <Form onSubmit={formik.handleSubmit}>
-                    <label>Nombre</label>
-                    {/* PARA PODER USAR CSS DEBO USAR CLASS NAME  */}
-                    <Field name="name" type="text" className="input" />
-                    {/* En caso de querer utilizar un text area debo utilizar la propiead de as pudiendo usar textarea select colocando las opciones entre etiquetas  */}
-                    <Field name="select" as="select" className="input" >
-                        <option value="a">Chachito Feliz</option>
-                        <option value="b">Chachito Triste </option>
-                    </Field>
-                    <ErrorMessage name="name"/>
-                    <br/>
-                    <label>Apellido</label>
-                    <Field name="lastname" type="text" />
-                    <ErrorMessage name="lastname"/>
-                    <br/>
-                    <label>Email</label>
-                    <Field name="email" type="email" />
-                    <ErrorMessage name="email"/>
-                    <button type="submit">Enviar</button>
+
+                        {/* Uso y creacion de un componente personalizado */}
+                        <TextInput name = "name" label="Nombre"/>
+                       
+                        {/* PARA PODER USAR CSS DEBO USAR CLASS NAME 
+                        <Field name="name" type="text" className="input" /> */}
+                        {/* En caso de querer utilizar un text area debo utilizar la propiead de as pudiendo usar textarea select colocando las opciones entre etiquetas  */}
+                        {/* <Field name="select" as="select" className="input" >
+                            <option value="a">Chachito Feliz</option>
+                            <option value="b">Chachito Triste </option>
+                        </Field> */}
+                        {/* <ErrorMessage name="name"/> */}
+                        <label>Apellido</label>
+                        <Field name="lastname" type="text" />
+                        <ErrorMessage name="lastname"/>
+                        <br/>
+                        <label>Email</label>
+                        <Field name="email" type="email" />
+                        <ErrorMessage name="email"/>
+                        <button type="submit">Enviar</button>
                     </Form>
         }
     </Formik>
---
+++ src/components/TextInput.js
@@ -0,0 +1,19 @@
+import { useField } from "formik"
+
+const TextInput = ({label,...props}) =>{
+    //en este caso en vez de usarse el componete field se usa el hook field para obtener las propieadades y poder personalizarlo
+    //para esto le paso las propieades que tome en el front al useField
+    const [field , meta] = useField(props)
+
+    console.log({field,meta})
+    return(
+        <div>
+            <label>{label}</label>
+            {/* al hacer destructuring mediante el spreat operator al pasarle el field traido del useField obtiene todas las propieades propias de un input */}
+            {/* En caso de querer modificar el onChange puedo hacerlo mediante ...props */ }
+            <input {...field} {...props}/> 
+            {meta.touched && meta.error ? <div>{meta.error}</div> : null}
+        </div>
+    )
+}
+export default TextInput
