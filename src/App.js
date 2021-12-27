b287eb10a5d277b2058ce4712dcd665e980d6fcb
Mario <mamerida2013@gmail.com>
2021-12-27 11:53:35 -0300

    Inicializacion de proyecto instalacion de la libreria formik primer ejemplo de uso de la misma

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {useFormik} from 'formik'
 function App() {
+  //uso de formik mediante un objeto
+  const formik = useFormik({  
+    //contiene los valores iniciales de nuestro formulario
+    initialValues:{
+      name: '',
+      lastname:'',
+      email:"",
+    },
+    //onSubmit recibe los valores y hace algo en el submit
+    onSubmit: values => console.log(values)
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    //en este caso en el form se le pasa la propieadad handleSubmit para poder sincronizar con el submit de formik
+    // a cada input se le pasa el tipo, el handleChange para que formik maneje el cambio 
+    // por ultimo se sincroniza el valor con el de formik
+    <form onSubmit={formik.handleSubmit}>
+      <label>Nombre</label>
+      <input
+        name="name"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.name}
+      />
+      <label>Apellido</label>
+      <input
+        name="lastname"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.lastname}
+      />
+      <label>Email</label>
+      <input
+        name="email"
+        type="email"
+        onChange={formik.handleChange}
+        value={formik.values.email}
+      />
+      <button type="submit">Enviar</button>
+    </form>
   );
 }
 
